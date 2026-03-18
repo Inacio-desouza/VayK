4be50147bca346f82983c802db14ba5b1ea43beb
Inacio-desouza <indesouza@davidson.edu>
2026-03-18 10:06:44 -0400

    activities page and views flow

--- frontend/vayk_frontend/src/router/index.js
+++ frontend/vayk_frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ActivitiesPage from '../views/ActivitiesPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: HomeView },
+    { path: '/activities', component: ActivitiesPage },
+  ],
 })
 
 export default router
---
+++ frontend/vayk_frontend/src/views/ActivitiesPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+    <p>Activities Page</p>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  min-height: 100vh;
+  width: 100%;
+}
+</style>
