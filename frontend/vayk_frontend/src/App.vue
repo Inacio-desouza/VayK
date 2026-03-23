2d30b65d67e05e65e8227199a985c0eea9588f93
Matthew Pearson <matthewpearson803@gmail.com>
2026-03-23 15:42:49 -0400

    Removed styling from App.vue

--- frontend/vayk_frontend/src/App.vue
+++ frontend/vayk_frontend/src/App.vue
@@ -6,34 +6,3 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style>
-html,
-body,
-#app {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  min-height: 100%;
-}
-
-body {
-  font-family: 'Inter', sans-serif;
-  background: linear-gradient(180deg, #0828a8 0%, #1c62ff 45%, #2aa6ff 100%);
-}
-
-input,
-textarea,
-button {
-  font-family: 'Inter', sans-serif;
-}
-
-::placeholder {
-  font-family: 'Inter', sans-serif;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
-</style>
