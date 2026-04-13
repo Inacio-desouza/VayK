8905bb49b98618f85cf7923e26bce20fd5b3de61
Sam <samtaylormoore@datamachines.io>
2026-04-12 20:57:55 -0400

    Make date boxes appear clickable, able to select by clicking whole button

--- frontend/vayk_frontend/src/components/form/StartDateInput.vue
+++ frontend/vayk_frontend/src/components/form/StartDateInput.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref } from 'vue'
+
 defineProps({
   modelValue: {
     type: String,
@@ -7,11 +9,24 @@ defineProps({
 })
 
 const emit = defineEmits(['update:modelValue'])
+
+const inputRef = ref(null)
+
+function openPicker() {
+  if (!inputRef.value) return
+
+  if (inputRef.value.showPicker) {
+    inputRef.value.showPicker()
+  } else {
+    inputRef.value.focus()
+  }
+}
 </script>
 
 <template>
-  <div class="date-input-wrapper">
+  <div class="date-input-wrapper" @click="openPicker">
     <input
+      ref="inputRef"
       :value="modelValue"
       @input="emit('update:modelValue', $event.target.value)"
       type="date"
@@ -22,16 +37,22 @@ const emit = defineEmits(['update:modelValue'])
 
 <style scoped>
 .date-input-wrapper {
-  position: relative;
   width: 100%;
+  cursor: pointer;
+}
+
+.date-input::-webkit-calendar-picker-indicator {
+  cursor: pointer;
 }
 
 .date-input {
-  width: 94%;
+  width: 100%;
+  box-sizing: border-box;
   padding: 12px;
   font-size: 14px;
   border: 1px solid #ccc;
   font-family: "Inter";
+  cursor: pointer;
 }
 
 .date-input:focus {
