d096ce04db1c0fdd67bd56b67b00f791eca5672e
JswimO5 <jmcdo1swim@gmail.com>
2026-04-27 15:32:05 -0400

    Updated the icon for the activity links.

--- frontend/vayk_frontend/src/components/activities_page/ActivityCard.vue
+++ frontend/vayk_frontend/src/components/activities_page/ActivityCard.vue
@@ -124,9 +124,9 @@ function handleRemove() {
         <p v-if="activity.url" class="activity-url">
           <a :href="activity.url" target="_blank" rel="noopener noreferrer">
             View on website
-            <svg viewBox="0 0 16 16" fill="none">
-              <path d="M12.5 8.5H3.5M12.5 8.5V3.5M12.5 8.5V13.5M3.5 8.5H13.5M3.5 8.5V3.5M3.5 8.5V13.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16" height="16">
+                <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
+              </svg>
           </a>
         </p>
       </div>
